6fa8e6ab1c74ebdd428f5c8f1fe3c0b415ba037e
Yuliia <yuliadzyoba@gmail.com>
2024-11-06 12:32:43 +0200

    add MovieDetails fixing

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -6,9 +6,6 @@ import axios from 'axios';
 import { getMovieById } from '../../services/api';
 import styles from './MovieDetailsPage.module.css';
 
-const defaultImg =
-  '<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>';
-
 const MovieDetailsPage = () => {
   const [movie, setMovie] = useState(null);
   const navigate = useNavigate();
@@ -46,12 +43,7 @@ const MovieDetailsPage = () => {
           Go back
         </button>
         <img
-          src={
-            movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-              : defaultImg
-          }
-          // src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.original_title}
         />
         <h2>{movie.title}</h2>
